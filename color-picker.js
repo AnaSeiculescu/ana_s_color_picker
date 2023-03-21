aff09ec111db5d23fa70083f720c0e3eb0150a03
AnaSeiculescu <122996287+AnaSeiculescu@users.noreply.github.com>
2023-03-21 07:51:46 +0200

    2 user can click on color blocks (#4)
    * 2: highlighting the hovered color box and adding spaces for the selected hue value and color
    
    * 2: adding hue value and color of the hue at the bottom, on click event
    
    * 2: making the border of a color box disapear at another color box click, remaining only one color box bordered

--- color-picker.js
+++ color-picker.js
@@ -8,11 +8,36 @@ for (let i = 0; i < 360; i += 10) {
     function createColorBox(i) {
 
         let colorBox = document.createElement("li");
+        colorBox.id = i;
         colorBox.style.backgroundColor = "hsl(" + i + ", 100%, 50%)";
         colorBox.style.height = Number(theBigRect.scrollHeight) / 36 - 3 + "px";
         return colorBox;
     }
 
     document.getElementById("set-color-rect").appendChild(createColorBox(i));
+    // console.log(createColorBox(i));
 
+}
+
+let hueNumber = document.getElementById("hue-number");
+// console.log(hueNumber);
+let selectedHue = document.getElementById("selected-hue");
+let textToDisappear = document.getElementById("text-to-disappear");
+console.log(textToDisappear);
+
+for (let j = 0; j < 360; j += 10) {
+    let colorBtn = document.getElementById(j);
+    colorBtn.addEventListener("click", function() {
+        for (let k = 0; k < 360; k += 10) {
+            let borderVerif = document.getElementById(k);
+            if (borderVerif.style.border == "2px solid black") {
+                // borderVerif.style.border = "0";
+                borderVerif.style.border = null;
+            }
+        }
+        hueNumber.innerText = j;
+        textToDisappear.style.display = 'none';
+        selectedHue.style.backgroundColor = colorBtn.style.backgroundColor;
+        colorBtn.style.border = "2px solid black";
+    })
 }
