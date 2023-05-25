2a64c8b8a6a59799493ead43adb1b11111ebec8b
AnaSeiculescu <122996287+AnaSeiculescu@users.noreply.github.com>
2023-05-25 09:14:10 +0100

    2 user can click on color blocks (#10)
    * 2: highlighting the hovered color box and adding spaces for the selected hue value and color
    
    * 2: adding hue value and color of the hue at the bottom, on click event
    
    * 2: making the border of a color box disapear at another color box click, remaining only one color box bordered
    
    * #6 applying css grid layout, hue swatches are stable when hovered
    
    * #6 remove colorBox height style in js
    
    * #5 user can select one variant of the selected hue, out of 64 variants, and the selected color it's visible on the bottom of the page

--- color-picker.js
+++ color-picker.js
@@ -1,7 +1,5 @@
 
 let theBigRect = document.getElementById("big-rect");
-// let styleTheBigBox = getComputedStyle(theBigRect);
-// console.log(styleTheBigBox);
 
 for (let i = 0; i < 360; i += 10) {
 
@@ -10,34 +8,103 @@ for (let i = 0; i < 360; i += 10) {
         let colorBox = document.createElement("li");
         colorBox.id = i;
         colorBox.style.backgroundColor = "hsl(" + i + ", 100%, 50%)";
-        colorBox.style.height = Number(theBigRect.scrollHeight) / 36 - 3 + "px";
         return colorBox;
     }
 
-    document.getElementById("set-color-rect").appendChild(createColorBox(i));
-    // console.log(createColorBox(i));
-
+    document.getElementById("batch-colors-rect").appendChild(createColorBox(i));
 }
 
 let hueNumber = document.getElementById("hue-number");
-// console.log(hueNumber);
-let selectedHue = document.getElementById("selected-hue");
-let textToDisappear = document.getElementById("text-to-disappear");
-console.log(textToDisappear);
+let selectedColor = document.getElementById("selected-color");
+let hueValue = document.getElementById("hue-value");
+// let textToDisappear = document.getElementById("text-to-disappear");
+
+let gridColors = document.getElementById("big-rect");
+let hslColorValue = document.getElementById("hsl-color-value");
 
 for (let j = 0; j < 360; j += 10) {
+
     let colorBtn = document.getElementById(j);
     colorBtn.addEventListener("click", function() {
         for (let k = 0; k < 360; k += 10) {
             let borderVerif = document.getElementById(k);
             if (borderVerif.style.border == "2px solid black") {
-                // borderVerif.style.border = "0";
                 borderVerif.style.border = null;
             }
         }
         hueNumber.innerText = j;
-        textToDisappear.style.display = 'none';
-        selectedHue.style.backgroundColor = colorBtn.style.backgroundColor;
+        // textToDisappear.style.display = 'none';
+        hueValue.style.backgroundColor = colorBtn.style.backgroundColor;
         colorBtn.style.border = "2px solid black";
+
+        if (gridColors.hasChildNodes()) {
+            gridColors.innerHTML = "";
+        }
+
+        createGridForHue(j);
+
     })
+
+
+
 }
+
+function createGridForHue(param) {
+
+    let gridColorItems = [];
+
+    for (let n = 0; n < 100; n += 12.5) {  //lightness
+
+        for (let m = 0; m <= 100; m += 14.2857) {   //saturation
+            
+            let item = []
+
+            let colorItem = document.createElement("div");
+            let saturation = Math.round(m);
+            colorItem.style.backgroundColor = `hsl(${param}, ${saturation}%, ${n}%)`;
+
+            item.push(colorItem);
+            item.push(`hsl(${param}, ${saturation}%, ${n}%)`);
+
+            gridColors.appendChild(colorItem);
+            gridColorItems.push(item);
+
+            // hslColorValue.innerText = `hsl(${param}, ${m}%, ${n}%)`;
+
+        }
+
+    }
+
+    console.log(gridColorItems);
+
+    for (let i = 0; i < gridColorItems.length; i++) {
+        gridColorItems[i][0].addEventListener("click", function() {
+            for (let j = 0; j < gridColorItems.length; j++) {
+                if (gridColorItems[j][0].style.border != "") {
+                    gridColorItems[j][0].style.border = "";
+                }
+            }
+            gridColorItems[i][0].style.border = "4px solid white";
+            selectedColor.style.backgroundColor = gridColorItems[i][0].style.backgroundColor;
+            selectedColor.innerText = gridColorItems[i][1];
+        })
+    }
+
+}
+
+let defaultHue = document.getElementById(0);
+
+jQuery(document).ready(function() {
+    jQuery(defaultHue).ready(function() {
+        jQuery(this).click();
+        hueNumber.innerText = 0;
+        // textToDisappear.style.display = 'none';
+        hueValue.style.backgroundColor = defaultHue.style.backgroundColor;
+        defaultHue.style.border = "2px solid black";
+
+        createGridForHue(0);
+    
+        console.log(gridColors);
+    });
+});
+
